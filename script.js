aa33761f3d83dd8b52b496b6c5b055d57a532fa0
aksenov-web <aksenov@inbox.lv>
2020-09-10 22:17:03 +0300

    todos

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 $('#new_task').keypress(function(event){
-
-	var keycode = (event.keyCode ? event.keyCode : event.which);
+	let keycode = (event.keyCode ? event.keyCode : event.which);
 	if(keycode == '13'){
     $('#task').val($('#new_task').val());
     $('#new_task').val('');
 	}
-	//event.stopPropagation();
 });
 
 
