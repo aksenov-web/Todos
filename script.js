eeb4a32535227c2b76c6a42fd8e0ba6dc68cb936
aksenov-web <aksenov@inbox.lv>
2020-09-10 22:41:54 +0300

    enter button:
    not accomplieshed:
    - clone div;
    
    x button function:
    not accomplieshed:
    - remove div;
    accomplieshed:
    - remove text from input field;

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 $('#new_task').keypress(function(event){
 	if(event.which == '13'){
+      //$('#task').clone().appendTo('.input-group');
       $('#task').val($('#new_task').val());
       $('#new_task').val('');
+
 	}
 });
+$('button').click(function() {
+  //$('#task').val('');
+  $('div').remove('#taskOne');
+})
 
 
 // function
