e193f96a7cb86902e23ea3224a5e74ea9e160438
aksenov-web <aksenov@inbox.lv>
2020-09-11 08:28:43 +0300

    Clone adjusted

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ $('.remove_button').click(function() {
 
 $('#new_task').keypress(function(event){
 	if(event.which == '13'){
-      $('#todo').append('<div class="todo_field input-group mb-3"><div class="input-group-prepend"><div class="input-group-text"><input type="checkbox" aria-label="Checkbox for following text input"></div></div><input id="task" type="text" class="form-control" aria-label="Text input with checkbox"><button type="button" class="remove_button btn btn-light">x</button></div>');
-      //$('#task').clone().appendTo('.todo');
+      //$('#todo').append('<div class="todo_field input-group mb-3"><div class="input-group-prepend"><div class="input-group-text"><input type="checkbox" aria-label="Checkbox for following text input"></div></div><input id="task" type="text" class="form-control" aria-label="Text input with checkbox"><button type="button" class="remove_button btn btn-light">x</button></div>');
+      $('.todo_field:last-child').clone().appendTo('#todo');
       $('#task').val($('#new_task').val());
       $('#new_task').val('');
 	}
