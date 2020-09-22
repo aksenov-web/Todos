39ee62683ab0480a88ff4a96a086c3dd288ec78e
aksenov-web <aksenov@inbox.lv>
2020-09-22 13:41:45 +0300

    by pressin enter appended new task with text

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-$('.remove_button').click(function() {
-  //$('input:text').val('');
-  //$('.todo_field').addClass('to_remove');
-  $('.todo_field').remove();
-})
-
 $('#new_task').keypress(function(event){
 	if(event.which == '13'){
-      //$('#todo').append('<div class="todo_field input-group mb-3"><div class="input-group-prepend"><div class="input-group-text"><input type="checkbox" aria-label="Checkbox for following text input"></div></div><input id="task" type="text" class="form-control" aria-label="Text input with checkbox"><button type="button" class="remove_button btn btn-light">x</button></div>');
-      $('.todo_field:last-child').clone().appendTo('#todo');
-      $('#task').val($('#new_task').val());
+      let comment = $('#new_task').val();
+      $('#todo').append('<div class="todo_field input-group mb-3"><div class="input-group-prepend"><div class="input-group-text"><input type="checkbox" aria-label="Checkbox for following text input"></div></div><input id="task" type="text" value="' + comment +'" class="form-control" aria-label="Text input with checkbox"><button type="button" class="remove_button btn btn-light">x</button></div>');
+      //$('.todo_field:last-child').clone().appendTo('#todo');
+      //$('#task').val($('#new_task').val());
       $('#new_task').val('');
 	}
 });
+
+$('.remove_button').click(function() {
+  alert("!");
+  //$('input:text').val('');
+  //$('.todo_field').addClass('to_remove');
+  //$('.todo_field').remove();
+})
