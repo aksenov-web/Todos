d3902d862e0a81f7fce0541b6c4db73db34ab12a
aksenov-web <aksenov@inbox.lv>
2020-09-22 15:10:48 +0300

    checkbox function (alert at the moment);
    visual appearance updated;

--- script.js
+++ script.js
@@ -20,14 +20,14 @@ console.log(count);
 
 for (let i = 1; i <= count; i++) {
   let comment = localStorage.getItem('comment_value(' + i + ')');
-  $('#todo').append('<div class="todo_field input-group mb-3"><div class="input-group-prepend"><div class="input-group-text"><input type="checkbox" aria-label="Checkbox for following text input"></div></div><input id="task" type="text" value="' + comment +'" class="form-control" aria-label="Text input with checkbox"><button type="button" class="remove_button btn btn-light">x</button></div>');
+  $('#todo').append('<div class="todo_field input-group mb-3"><div class="input-group-prepend"><div class="input-group-text"><input class="checkbox" type="checkbox" aria-label="Checkbox for following text input"></div></div><input id="task" type="text" value="' + comment +'" class="form-control" aria-label="Text input with checkbox"><button type="button" class="remove_button btn btn-light">x</button></div>');
 }
 
 $('#new_task').keypress(function(event){
 	if(event.which == '13'){
       let comment = $('#new_task').val();
       let count = Number(localStorage.getItem('comment_count'));
-      $('#todo').append('<div class="todo_field input-group mb-3"><div class="input-group-prepend"><div class="input-group-text"><input type="checkbox" aria-label="Checkbox for following text input"></div></div><input id="task" type="text" value="' + comment +'" class="form-control" aria-label="Text input with checkbox"><button type="button" class="remove_button btn btn-light">x</button></div>');
+      $('#todo').append('<div class="todo_field input-group mb-3"><div class="input-group-prepend"><div class="input-group-text"><input class="checkbox" type="checkbox" aria-label="Checkbox for following text input"></div></div><input id="task" type="text" value="' + comment +'" class="form-control" aria-label="Text input with checkbox"><button type="button" class="remove_button btn btn-light">x</button></div>');
       //$('.todo_field:last-child').clone().appendTo('#todo');
       //$('#task').val($('#new_task').val());
       $('#new_task').val('');
@@ -43,6 +43,12 @@ $('.remove_button').click(function() {
   //$('.todo_field').remove();
 })
 
+$('.checkbox').click(function() {
+  alert("!");
+  //$('input:text').val('');
+  //$('.todo_field').addClass('to_remove');
+  //$('.todo_field').remove();
+})
 
 
 
