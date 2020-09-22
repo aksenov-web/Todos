895b43374e5fcecbd7e1a6d88f6c14640c1d7a41
aksenov-web <aksenov@inbox.lv>
2020-09-22 16:30:41 +0300

    minor changes

--- script.js
+++ script.js
@@ -11,37 +11,34 @@ if (count === null) {
   count = 0;
 }
 
-$('#clear').click(function() {
+$('.clear').click(function() {
   localStorage.clear();
   location.reload();
   });
 
-console.log(count);
+//console.log(count);
 
 for (let i = 1; i <= count; i++) {
   let comment = localStorage.getItem('comment_value(' + i + ')');
   $('#todo').append('<div data-order="' + count + '"  class="todo_field input-group mb-3"><div class="input-group-prepend"><div class="input-group-text"><input class="checkbox" type="checkbox" aria-label="Checkbox for following text input"></div></div><input type="text" value="' + comment +'" class="form-control" aria-label="Text input with checkbox"><button type="button" class="remove_button btn btn-light">x</button></div>');
 }
 
-
-
 $('#new_task').keypress(function(event){
-	if(event.which == '13'){
-      let comment = $('#new_task').val();
-      let count = Number(localStorage.getItem('comment_count'));
-      localStorage.setItem('comment_count', ++count);
-      $('#todo').append('<div data-order="' + count + '" class="todo_field  input-group mb-3"><div class="input-group-prepend"><div class="input-group-text"><input class="checkbox" type="checkbox" aria-label="Checkbox for following text input"></div></div><input type="text" value="' + comment +'" class="form-control" aria-label="Text input with checkbox"><button type="button" class="remove_button btn btn-light">x</button></div>');
-      //$('.todo_field:last-child').clone().appendTo('#todo');
-      //$('#task').val($('#new_task').val());
-      $('#new_task').val('');
+  if(event.which == '13'){
+    let comment = $('#new_task').val();
+    let count = Number(localStorage.getItem('comment_count'));
+    localStorage.setItem('comment_count', ++count);
+    $('#todo').append('<div data-order="' + count + '" class="todo_field  input-group mb-3"><div class="input-group-prepend"><div class="input-group-text"><input class="checkbox" type="checkbox" aria-label="Checkbox for following text input"></div></div><input type="text" value="' + comment +'" class="form-control" aria-label="Text input with checkbox"><button type="button" class="remove_button btn btn-light">x</button></div>');
+    //$('.todo_field:last-child').clone().appendTo('#todo');
+    //$('#task').val($('#new_task').val());
+    $('#new_task').val('');
 
-      localStorage.setItem('comment_value(' + count + ')', comment);
-	}
+    localStorage.setItem('comment_value(' + count + ')', comment);
+  }
 });
 
 $('.remove_button').click(function() {
-  if ($(this).parent().parent().parent().find('[type="text"]').attr('disabled')) {
-
+  if ($(this).parent().find('[type="text"]').attr('disabled')) {
   }
   else {
     $(this).parent().find('[type="text"]').val('');
